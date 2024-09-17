176ebd66285e6b2611331107be8e5c3b52c834e6
Hiten <hitengamer11@gmail.com>
2024-09-17 22:58:12 +0545

    seller page modified

--- frontend/src/Pages/SellerHome.jsx
+++ frontend/src/Pages/SellerHome.jsx
@@ -6,13 +6,13 @@ import SellerNavbar from "../Seller/SellerNavbar";
 export default function SellerHome() {
   return (
     <div className="overflow-hidden">
-      <SellerNavbar />
+      <SellerNavbar/>
       <div className="flex gap-3 h-[100vh]">
         {/* Sidebar */}
         <SellerSideBar />
 
         {/* Main content (Outlet) */}
-        <div className="flex-grow p-4 overflow-auto">
+        <div className="flex-grow p-4 overflow-y-auto">
           <Outlet />
         </div>
       </div>
